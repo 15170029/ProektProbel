5a9b5edda926f4e062ca9e3f370c59c5b56ecb3e
15170029 <aleksei.domrachev@mail.ru>
2019-04-02 23:44:54 +0500

    proekt

---
+++ ProektProbel/src/main/java/ProektProbel/ProektProbel/ProektProbel.java
@@ -0,0 +1,19 @@
+package ProektProbel.ProektProbel;
+
+import java.util.Scanner;
+
+public class ProektProbel {
+ 
+    public static void main(String[] args) {
+       
+        Scanner in = new Scanner(System.in);
+        System.out.print("Input string: ");
+        String stroka = in.nextLine();
+        int p=0;
+        for(char i : stroka.toCharArray()){
+            if(i == ' ') p++;
+        }
+        System.out.printf("Kolichestvo probelov: %d", p);
+        in.close();
+    }   
+}
